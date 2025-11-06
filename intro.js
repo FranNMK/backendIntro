2445ada7750846d34032c472ad5c4e0379bfd9ce
Francis <frankmk2025@gmail.com>
2025-11-06 08:30:28 +0300

    My first update

---
+++ intro.js
@@ -0,0 +1,10 @@
+let Details = {
+    name: "Your Name",
+    age: 25,
+    favProgrammingLanguage: "JavaScript"
+};
+
+// Display the information
+console.log("My name is: " + Details.name);
+console.log("I am " + Details.age + " years old");
+console.log("My favorite programming language is: " + Details.favProgrammingLanguage);
